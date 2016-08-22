d3578c7af69b8ab126e2163f1acdc20d29832f27
Dario Caruso <darioyzf@gmail.com>
2016-08-22 02:32:36 +0200

    up

--- test/main.ts
+++ test/main.ts
@@ -12,11 +12,11 @@ inMilliSeconds=UT('milliseconds')
 })
 describe("uptime formats", () => {
 
-    it("seconds", () => {
+    it("in seconds are "+inSeconds+' seconds', () => {
         expect(inSeconds).to.be.a('number')
         expect(inSeconds.toString().split('.').length).to.be.eq(1)
     })
-    it("milliseconds", () => {
+    it("in milliseconds are "+inMilliSeconds+' milliseconds', () => {
         expect(inMilliSeconds).to.be.a('number')
         expect(inSeconds).to.be.equal(parseInt((inMilliSeconds/1000).toString()))
     })
